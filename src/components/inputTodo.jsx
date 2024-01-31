98e0088132cbec77d94de5e5e686b6964a01d956
go <goito.722@icloud.com>
2024-01-31 17:56:37 +0900

    fix css inputTodo

--- src/components/inputTodo.jsx
+++ src/components/inputTodo.jsx
@@ -1,6 +1,14 @@
+const style = {
+    backgroundColor: "#c6e5d9",
+    width:"400px",
+    height:"30px",
+    padding:"8px",
+    margin: "8px",
+    borderRadius: "8px",
+};
 export const InputTodo = (props) =>{
     const {todoText,onChange,onClick} = props;
-    return(<div className="input-area">
+    return(<div style = {style}>
         <input placeholder="TODOを入力" value={todoText} onChange={onChange}></input>
         <button onClick = {onClick}>追加</button>
       </div>
